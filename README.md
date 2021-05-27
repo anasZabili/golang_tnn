942a786a884fd3a89cef56d2aad057d9973d660f
anas zabili <anaszab78@gmail.com>
2021-05-27 20:58:29 +0200

    lesson 5 array and slice

--- README.md
+++ README.md
@@ -8,3 +8,21 @@ fmt.PrintF format:
 
 fmt.Sprintf: 
   enregistre des chaine de caractere formatté dans une varibale
+
+Array: 
+  var ages2 = [3]int{20, 22, 32} = tout comme en C la taille est fixe
+
+Slice:
+  Fonctionne sur la base d'un array mais permet de modifier le taille 
+  et met a disposition la fonction append(slice, element)
+  qui renvoie un tableau contenant le nouvelle element
+  var scores = []int{1, 2, 3}
+  scores2 := append(scores, 100)
+
+Range: 
+  Permet de faire une recopie de tableau a un indice donné borne sup non incluse
+  rangeOne := names[1:3]
+	rangeTwo := names[0:]
+	rangeThree := names[:2]
+  NOn destrucive et retourn un slice, ainsi on peut creer un slice a
+  a partir d'un array avec un range
--- main.go
+++ main.go
@@ -3,23 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	// Print
-	fmt.Print("hello world")
 
-	// Println
-	age := 22
-	name := "anas"
-	height := 1.77
-	fmt.Println("hello")
-	fmt.Println("my age is", age, "my name is", name)
+	// array taille fixe
+	var ages [3]int = [3]int{20, 22, 32}
+	var ages2 = [3]int{20, 22, 32}
+	names := [3]string{"mario", "warrio", "luigi"}
+	names[1] = "anas"
+	fmt.Println(ages)
+	fmt.Println(ages2, len(ages2))
+	fmt.Println(names, len(names))
 
-	// Printf Formated string
-	fmt.Printf("my name is %v my age is %v \n", name, age)
-	fmt.Printf("my name is %q my age is %d \n", name, age)
-	fmt.Printf("age is of type %T \n", age)
-	fmt.Printf("his height is %0.2f \n", height)
+	// slices array dynamique
+	var scores = []int{1, 2, 3}
+	scores[2] = 25
+	scores2 := append(scores, 100)
+	fmt.Println(scores)
+	fmt.Println(scores2)
 
-	// SprintF
-	description := fmt.Sprintf("my age is %v and my name is %v", age, name)
-	fmt.Printf("%v", description)
+	// slice ranges
+
+	rangeOne := names[1:3]
+	rangeTwo := names[0:]
+	rangeThree := names[:2]
+	fmt.Println("a range", rangeOne)
+	fmt.Println("a range", rangeTwo)
+	fmt.Println("a range", rangeThree)
 }
